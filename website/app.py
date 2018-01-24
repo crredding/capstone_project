40eea2f11db50c4ab1d354ea6d0169e9914b75a7
Charles Redding <crredding@gmail.com>
2018-01-24 13:00:57 -0800

    updates to website

--- website/app.py
+++ website/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from recommender_model import RecommenderModel
 
-app = Flask (__name__)
+app = Flask(__name__)
 df = pd.read_csv('../data/df_with_features.csv', index_col=0)
 mapping_df = pd.read_csv('../data/mapping_df.csv', index_col=0)
 model = RecommenderModel(df, mapping_df)
@@ -11,10 +11,14 @@ model = RecommenderModel(df, mapping_df)
 #This is just for testing, before lat and lng incorporated into the model
 lat, lng = 47.612133, -122.335908
 
+@app.route('/')
+def index():
+    return render_template('main.html')
+
 @app.route('/submit', methods=['POST'])
 def submit():
     print('submitting')
-    data = requst.json
+    data = request.json
     f1_name, f2_name, f3_name = str(data[f1_name], data[f2_name], data[f3_name])
     f1_weight, f2_weight, f3_weight = float(data[f1_weight], data[f2_weight],
                                           data[f3_weight])
@@ -23,10 +27,6 @@ def submit():
     f3 = tuple(f3_weight, f3_name)
     recommendations = model.recommend(f1, f2, f3, lat, lng)
     return jsonify(recommendations)
-
-@app.route('/')
-def index():
-    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True, debug=True)
