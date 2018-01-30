cde014ecff08678df66170181285d63843c8dc03
Charles Redding <crredding@gmail.com>
2018-01-30 08:57:46 -0800

    Moved google_api_functions

--- website/app.py
+++ website/app.py
@@ -2,22 +2,17 @@ from flask import Flask, render_template, request, jsonify, redirect, send_file
 import pandas as pd
 import yaml
 from recommender_model import RecommenderModel
-from google_api_functions import *
 from io import BytesIO
 from pathlib import Path
 import base64
 
-with open('/Users/ReddingSkinnyRobot/.secrets/google_api.yaml') as f:
-    google_secrets = yaml.load(f)
+default_lat = 47.6130285
+default_lng = -122.3420645
 
 app = Flask(__name__)
 df = pd.read_csv('../data/df_with_features.csv', index_col=0)
 mapping_df = pd.read_csv('../data/mapping_df.csv', index_col=0)
 model = RecommenderModel(df, mapping_df)
-
-with open('static/images/default_shop_image.jpg', 'rb') as f1:
-    f2 = f1.read()
-    default_shop_image = base64.b64encode(bytearray(f2))
 
 @app.route('/')
 def index():
@@ -34,14 +29,17 @@ def tech():
 #This works with form input
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    print(request.form['coord'])
     f1 = tuple((float(request.form['f1_weight']), request.form['feature1']))
     f2 = tuple((float(request.form['f2_weight']), request.form['feature2']))
     f3 = tuple((float(request.form['f3_weight']), request.form['feature3']))
-    lat = request.form['coord'].split(',')[0]
-    lng = request.form['coord'].split(',')[1]
     r = float(request.form['range'])
-    print(r)
+    if len(request.form['coord']) > 0:
+        lat = request.form['coord'].split(',')[0]
+        lng = request.form['coord'].split(',')[1]
+    else:
+        lat = default_lat
+        lng = default_lng
+        r = 20
     recs = model.recommend(f1, f2, f3, lat, lng, r).to_dict('records')
     for rec in recs:
         rec['split_address'] = rec['address'].replace(' ', '+') + '+seattle'
